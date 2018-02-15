cdcd3d6d9c697fd70bc8a0b4508ab307a95734e0
Andries van der Walt <andriesvanderwalt@gmail.com>
2018-02-15 23:41:36 +0200

    Add files via upload

---
+++ Gather/SQL Code/population_manipulation_updated.sql
@@ -0,0 +1,71 @@
+
+-- add columns to the table which add the demographic of each of the races 
+ALTER TABLE population ADD COLUMN total_id VARCHAR(20);
+
+
+-- Add unique UUids to the population table
+ALTER TABLE population ADD COLUMN population_key UUid;
+
+-- update the values of these columns
+UPDATE population SET total_id = 'total';
+
+SELECT * FROM population
+
+--Using a union, make the indivdual tables for each of total, ethnic groups, genders, and languages
+--total
+DROP TABLE IF EXISTS new_database.population_total;
+SELECT 
+population_key, ty.year_key, sb.suburb_key, total AS "count", total_id as demographic
+INTO new_database.population_total
+FROM population as pop
+LEFT JOIN new_database.t_year as ty
+ON pop."year" = ty.t_year
+LEFT JOIN new_database.suburbs as sb
+ON pop.suburb = sb.suburb_pop;
+
+SELECT * FROM new_database.population_total;
+
+ALTER TABLE new_database.population_total ADD CONSTRAINT unique_population_total_key UNIQUE(population_key);
+
+-- make demographic table total
+DROP TABLE IF EXISTS new_database.demographic_total;
+
+SELECT uuid_generate_v4() as demographic_key,
+demographic 
+INTO new_database.demographic_total
+FROM (SELECT DISTINCT demographic FROM new_database.population_total) as subtable;
+
+ALTER TABLE new_database.demographic_total 
+	ADD CONSTRAINT unique_demographic_total_key UNIQUE(demographic_key),
+    ADD PRIMARY KEY (demographic_key);
+
+SELECT * FROM new_database.demographic_total;
+
+ALTER TABLE new_database.population_total DROP COLUMN demographic;
+ALTER TABLE new_database.population_total ADD COLUMN demographic_key UUid;
+
+UPDATE new_database.population_total SET demographic_key = dt.demographic_key
+FROM new_database.demographic_total AS dt;
+
+SELECT * FROM new_database.population_total;
+
+ALTER TABLE new_database.population_total
+	ADD CONSTRAINT lnk_t_year_population_total FOREIGN KEY(year_key)
+    REFERENCES new_database.t_year (year_key) MATCH FULL
+    ON DELETE Restrict
+    ON UPDATE Cascade;
+    
+ALTER TABLE new_database.population_total
+	ADD CONSTRAINT lnk_suburb_population_total FOREIGN KEY(suburb_key)
+    REFERENCES new_database.suburbs (suburb_key) MATCH FULL
+    ON DELETE Restrict
+    ON UPDATE Cascade;
+    
+ALTER TABLE new_database.population_total
+	ADD CONSTRAINT lnk_demographic_population_total FOREIGN KEY(demographic_key)
+    REFERENCES new_database.demographic_total (demographic_key) MATCH FULL
+    ON DELETE Restrict
+    ON UPDATE Cascade;
+    
+ALTER TABLE new_database.population_total
+	ADD PRIMARY KEY( population_key);
